78f7ac1f13316d4e9692cd2a29d3067b816512ef
Виталий <saifutdinov_vitalii@mail.ru>
2025-10-27 20:22:46 +0500

    fdfsf

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', (e)=> {
+    //Открытие окна
+    const informationHide = document.querySelector('.information__wrapper-hide');
+    const informationBtn = document.querySelector('.information__btn');
+
+    informationBtn.textContent = `Читать далее`;
+    informationBtn.addEventListener('click', (e) => {
+        informationHide.classList.toggle('hide');
+        if (informationHide.classList.contains('hide')) {
+            informationBtn.textContent = `Читать далее`;
+        } else { 
+            informationBtn.textContent = `Скрыть текст`;
+        }
+    });
+   
+    
+})
