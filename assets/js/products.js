630004ad83d9bcee97d0ccafdfa7e6814f7e58a3
Виталий <saifutdinov_vitalii@mail.ru>
2025-11-11 16:42:44 +0500

    gfdg

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', ()=> {
-    const products = document.querySelectorAll('.products');
+    const products = document.querySelectorAll('.product');
     if (products) {
         products.forEach(el => {
             let currentProduct = el
@@ -10,16 +10,12 @@ document.addEventListener('DOMContentLoaded', ()=> {
                     el.setAttribute ('data-index', index);
                     imagePagination.innerHTML += `<li class="product__pagination-item ${index === 0 ? 'product__pagination-item--active': ''}" data-index="${index}"></li>`;
                     el.addEventListener('mouseenter', (e)=> {
-                        // currentProduct.classList.remove('product__pagination-item--active');
-                        // currentProduct.querySelector(`.product__pagination-item[data-index="${e.currentTarget.dataset.index}"]`).classList.add('product__pagination-item--active');
                         currentProduct.querySelectorAll('.product__pagination-item').forEach((el)=> {
                             el.classList.remove('product__pagination-item--active');
                             currentProduct.querySelector(`.product__pagination-item[data-index="${e.currentTarget.dataset.index}"]`).classList.add('product__pagination-item--active');
                         })
                     });
                     el.addEventListener('mouseleave', (e)=> {
-                        // currentProduct.classList.remove('product__pagination-item--active');
-                        // currentProduct.querySelector(`.product__pagination-item[data-index="${e.currentTarget.dataset.index}"]`).classList.add('product__pagination-item--active');
                         currentProduct.querySelectorAll('.product__pagination-item').forEach((el)=> {
                             el.classList.remove('product__pagination-item--active');
                             currentProduct.querySelector(`.product__pagination-item[data-index="0`).classList.add('product__pagination-item--active');
