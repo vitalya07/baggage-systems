3851ed4a0d905db3b85e3932b4774b8c72161298
Виталий <saifutdinov_vitalii@mail.ru>
2025-11-15 12:55:25 +0500

    gfdgfdg

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', ()=> {
+    //Прилипание фильтра
     const filter = document.querySelector('.fillter');
     const filterBox = document.querySelector('.fillter__box');
     const boxes = document.querySelector('.luggage__boxes').offsetHeight;
     const products = document.querySelector('.products').offsetHeight;
     i = boxes + products;
     filter.style.height = `${i}px`;
-    console.log(filter.offsetHeight)
     function filterFixed() {
         if(window.scrollY > 150) {
             filterBox.style.position = 'sticky';
@@ -21,4 +21,38 @@ document.addEventListener('DOMContentLoaded', ()=> {
     window.addEventListener('scroll', ()=> {
         filterFixed();
     });
-})
+    //Прилипание фильтра конец   
+    const rangeSlider = document.querySelector('#range-slider');
+    if(rangeSlider) {
+        noUiSlider.create(rangeSlider , {
+            start: [300, 999999],
+            connect: true,
+            step: 1,
+            range: {
+                'min': 300,
+                'max': 55000
+            }
+        });
+        const minPrice = document.querySelector('#min-price');
+        const maxPrice = document.querySelector('#max-price');
+
+        const prices = [minPrice, maxPrice];
+        
+        rangeSlider.noUiSlider.on('update', function(values, handel) {
+            prices[handel].value = Math.round(values[handel]);
+        });
+
+        const setRangeSlider = (i, value) => {
+            let arr = [null, null];
+            arr[i] = value;
+
+            rangeSlider.noUiSlider.set(arr)
+        }
+
+        prices.forEach((el, index) => {
+            el.addEventListener('change', (e)=> {
+                setRangeSlider(index, e.currentTarget.value)
+            })
+        })
+    }
+});
